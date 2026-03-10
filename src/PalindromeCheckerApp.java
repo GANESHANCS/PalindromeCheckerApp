f45f9429babc275c0149eb7822d39a3250c3e3e4
GANESHAN05 <ganeshancs5@gmail.com>
2026-03-10 08:53:06 +0530

    UC5 : Stack based palindrome number

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,17 +1,26 @@
+import java.util.Stack;
+
 public class PalindromeCheckerApp {
 
-    // Main Method - Application Entry Point
     public static void main(String[] args) {
 
-        // Application Welcome Message
-        System.out.println("=================================");
-        System.out.println("     Palindrome Checker App      ");
-        System.out.println("=================================");
-        System.out.println("Version : 1.0");
-        System.out.println("Welcome to the Palindrome Checker!");
-        System.out.println("---------------------------------");
+        String input = "noon";
+        Stack<Character> stack = new Stack<>();
 
-        // Program continues to next step
-        System.out.println("Ready to check palindrome...");
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+        boolean isPalindrome = true;
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        if (isPalindrome) {
+            System.out.println("Input:"+input + "\nIs it a palindrome? : Yes");
+        } else {
+            System.out.println("Input:"+input + "\nIs it a palindrome? : No");
+        }
     }
 }
